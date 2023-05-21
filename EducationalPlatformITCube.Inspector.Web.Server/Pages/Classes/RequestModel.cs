014f18720f2f0ab0be247e74d256ebe949a8a76c
leeeeeeeeeemon <emilbarish@gmail.com>
2023-05-21 15:19:37 +0300

    add RequestPage

---
+++ EducationalPlatformITCube.Inspector.Web.Server/Pages/Classes/RequestModel.cs
@@ -0,0 +1,24 @@
+﻿namespace EducationalPlatformITCube.Inspector.Web.Server.Pages.Classes
+{
+    public class RequestModel
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string ChildLastName { get; set; }
+        public string ChildFirstName { get; set; }
+        public string ChildMiddleName { get; set; }
+        public DateTime ChildBirthDate { get; set; }
+        public int Age { get; set; }
+        public string SchoolNumber { get; set; }
+        public string Grade { get; set; }
+        public string StudyShift { get; set; }
+        public int CourseSelectionNumber { get; set; }
+        public string FormEducation { get; set; }
+        public string ParentLastName { get; set; }
+        public string ParentFirstName { get; set; }
+        public string ParentMiddleName { get; set; }
+        public string ParentPhoneNumber { get; set; }
+        public string ParentComment { get; set; }
+        public string AdminComment { get; set; }
+    }
+}
